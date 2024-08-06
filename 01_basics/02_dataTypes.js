d658fa2ba59d9c0804fde73751825bb2a93a660c
amitagautam10 <166156127+amitagautam10@users.noreply.github.com>
2024-08-06 13:27:02 +0000

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+// code readability should be high
+
+let name = "amita"
+let age = 21
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigInt => trading, stock market
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object 
+
+console.log(typeof "amita");    //String
+console.log(typeof age);        //number
+console.log(typeof null);       //object
+console.log(typeof undefined);  //undefined
+
+
+
+
