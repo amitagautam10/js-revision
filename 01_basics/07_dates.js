da1c7c74957b1142f062a592decb071a735d8153
amitagautam10 <166156127+amitagautam10@users.noreply.github.com>
2024-08-07 14:56:43 +0000

    Date in js

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,41 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+// let myCreatedDate = new Date(2024,7,7)
+// let myCreatedDate = new Date(2024, 7, 7, 20, 11)
+// let myCreatedDate = new Date("2024-08-07")
+let myCreatedDate = new Date("07-08-2024")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(myDate.getMonth()+1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
