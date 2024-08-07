002593cc8faf697f2001fa1ab073019d7803994e
amitagautam10 <166156127+amitagautam10@users.noreply.github.com>
2024-08-07 14:29:59 +0000

    Math and Numbers

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,49 @@
+const score = 400
+
+const balance = new Number(100)
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const number = 23.6699
+// console.log(number.toPrecision(3));
+// console.log(number.toPrecision(2));
+
+const number1 = 1000000
+// console.log(number1.toLocaleString('en-IN'));
+
+// **********Maths************
+
+// console.log(Math);
+// console.log(Math.abs(-6));   //negative to positive vice-versa in not true
+// console.log(Math.round(6.5));
+// console.log(Math.ceil(6.2));
+// console.log(Math.floor(6.9));
+// console.log(Math.min(4,5,1,3,7,8));
+// console.log(Math.max(3,6,4,1,7,9));
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10+1));
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+
+
+
+
+
+
+
+
