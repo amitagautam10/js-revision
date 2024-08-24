2f556b131ac260dabd8d9092b8820cff7f1a061a
amitagautam10 <166156127+amitagautam10@users.noreply.github.com>
2024-08-24 13:49:34 +0000

    If & If_else

---
+++ 04_control_flow/01_if_else.js
@@ -0,0 +1,62 @@
+// if(condition){    condition must true then only the if block code execute
+
+// }
+const isUserLoggedIn = true
+const temp = 41
+const score = 200
+// if(isUserLoggedIn){
+//     console.log("User logged In");
+    
+// }
+// if( 2 === "2"){
+//     console.log("Executed");
+    
+// }
+// if(temp < 50){
+//     console.log("Less than 50");
+// }
+// else{
+//     console.log("Greater than 50");
+// }
+// console.log("executed");
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`); 
+// }
+// console.log(`User power: ${power}`);
+
+
+const balance = 1000
+// if(balance > 500)
+//     console.log("true");
+
+// if (balance > 500) 
+//     console.log("true"),
+//     console.log("executed");
+
+// if(balance<500){
+//     console.log("less than 500");
+// }
+// else if(balance<750){
+//     console.log("less than 750");   
+// }
+// else if(balance<900){
+//     console.log("less than 900");
+// }
+// else {
+//     console.log("Greater than or equal to 1000");
+// }
+
+const userLoggdIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromGmail = true
+if(userLoggdIn && debitCard && 2===3){
+    console.log("Allow to buy");
+}  
+if(loggedInFromGmail || loggedInFromGoogle){
+    console.log("User Logged In"); 
+}  
+    
+    
