229ce3dfd78c55825ada59214c6e1683802f8800
amitagautam10 <166156127+amitagautam10@users.noreply.github.com>
2024-09-12 14:49:00 +0000

    Classes and Object

---
+++ 10_classes_oop/01_oop.js
@@ -0,0 +1,36 @@
+const user = {
+    username: "amita",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        // console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+}
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+// const promise1 = new Promise()
+// const date = new Date()
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+    }
+    
+    return this
+}
+
+const user1 = new User("amita", 12, true)
+const user2 = new User("gautam", 11, false)
+console.log(user1.constructor);
+// console.log(user2);
+
---
+++ 10_classes_oop/notes.md
@@ -0,0 +1,23 @@
+# javascript and classes
+
+## OOP 
+
+## Object 
+- collection of properties and methods
+- toLowerCase, toUpperCase
+
+## why use OOP
+
+## parts of OOP
+- Object Literal
+
+- Constructor function
+- Prototypes
+- Classes 
+- Instances or new or this
+
+## 4 pillars of OOP
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
